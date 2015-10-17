da05e3147a358840d3edaf5827b5f89d99926cd3
Imran Ziad <imranziad@gmail.com>
2015-10-18 01:30:43 +0600

    template for contest

---
+++ template.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define LL long long
+#define NL '\n'
+#define xx first
+#define yy second
+#define pb push_back
+#define mp make_pair
+#define pii pair<int,int>
+#define FOR(i,j,k) for(i=j;i<=k;i++)
+#define REV(i,j,k) for(i=j;i>=k;i--)
+#define READ(f) freopen(f,"r",stdin)
+#define WRITE(f) freopen(f,"w",stdout)
+#define MOD 1000000007
+#define MAX 100005
+
+int main()
+{
+    //READ("in.txt");
+    //WRITE("out.txt");
+    std::ios_base::sync_with_stdio(0);
+    int cases, caseno=0, n, i, k, cnt;
+    pii p;
+
+    cin >> cases;
+
+    while(cases--)
+    {
+        cin >> n;
+
+
+    }
+
+    return 0;
+}
