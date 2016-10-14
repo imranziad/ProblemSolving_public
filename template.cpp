83f5874805c33d815a894636aabdb2574c2884d0
IMRAN <imranziad@gmail.com>
2016-10-14 10:36:26 +0600

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -8,28 +8,39 @@ using namespace std;
 #define pb push_back
 #define mp make_pair
 #define pii pair<int,int>
+#define si(x) scanf("%d",&x)
+#define sl(x) scanf("%I64d",&x)
+#define sd(x) scanf("%lf",&x)
+#define ss(x) scanf("%s",x)
 #define mem(a,b) memset(a,b,sizeof(a))
 #define FOR(i,j,k) for(i=j;i<=k;i++)
+#define REP(i,j,k) for(int i=j;i<=k;i++)
 #define REV(i,j,k) for(i=j;i>=k;i--)
 #define READ(f) freopen(f,"r",stdin)
 #define WRITE(f) freopen(f,"w",stdout)
+#define cnd tree[idx]
+#define lnd tree[idx*2]
+#define rnd tree[(idx*2)+1]
+#define lndp (idx*2),(b),((b+e)/2)
+#define rndp (idx*2+1),((b+e)/2+1),(e)
+#define pi 2.0*acos(0.0)
 #define MOD 1000000007
 #define MAX 100005
 
 int main()
 {
-    //READ("in.txt");
+    //READ("input.txt");
     //WRITE("out.txt");
-    std::ios_base::sync_with_stdio(0);
-    int cases, caseno=0, n, i, k, cnt;
+    int cases, caseno=0, n, i, j, k, cnt, sum;
 
-    cin >> cases;
+    scanf("%d", &cases);
 
     while(cases--)
     {
-        cin >> n;
+        scanf("%d", &n);
 
 
+        printf("Case %d:\n", ++caseno);
     }
 
     return 0;
