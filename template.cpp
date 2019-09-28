bd58991e187d7357bffccb83895c79a5d7bde44e
IMRAN <imranziad@gmail.com>
2019-09-28 10:12:07 +0600

    Updated with latest template for icpc

--- template.cpp
+++ template.cpp
@@ -1,36 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define LL long long
 #define NL '\n'
 #define xx first
 #define yy second
 #define pb push_back
 #define mp make_pair
 #define pii pair<int,int>
+#define ones __builtin_popcount
 #define si(x) scanf("%d",&x)
 #define sl(x) scanf("%I64d",&x)
 #define sd(x) scanf("%lf",&x)
 #define ss(x) scanf("%s",x)
 #define mem(a,b) memset(a,b,sizeof(a))
-#define FOR(i,j,k) for(i=j;i<=k;i++)
-#define REP(i,j,k) for(int i=j;i<=k;i++)
-#define REV(i,j,k) for(i=j;i>=k;i--)
+#define FOR(i,j,k) for(int i=j;i<=k;i++)
+#define REV(i,j,k) for(int i=j;i>=k;i--)
 #define READ(f) freopen(f,"r",stdin)
 #define WRITE(f) freopen(f,"w",stdout)
+#define debug(x) cerr << "debug = " << x << "\n";
+#define debugs(s,x) cerr << "debug : " << s << " = " << x << "\n";
 #define cnd tree[idx]
 #define lnd tree[idx*2]
 #define rnd tree[(idx*2)+1]
 #define lndp (idx*2),(b),((b+e)/2)
 #define rndp (idx*2+1),((b+e)/2+1),(e)
+#define X real()
+#define Y imag()
+#define Point complex<long double>
 #define pi 2.0*acos(0.0)
+#define EPS 1e-9
+#define INF INT_MAX
 #define MOD 1000000007
 #define MAX 100005
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
 
 int main()
 {
     //READ("input.txt");
     //WRITE("out.txt");
+    //mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+    //uniform_int_distribution<int>(0, 10000)(rng)
+    //std::ios_base::sync_with_stdio(0);
     int cases, caseno=0, n, i, j, k, cnt, sum;
 
     scanf("%d", &cases);
